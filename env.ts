090a1b089ac651a7a3260c0e67a9a4aa6ddfd183
nicholasscabral <nicholasscabral@gmail.com>
2021-11-05 10:34:47 -0300

    configure mysql

--- env.ts
+++ env.ts
@@ -15,8 +15,11 @@
 import Env from "@ioc:Adonis/Core/Env";
 
 export default Env.rules({
-  HOST: Env.schema.string({ format: "host" }),
-  PORT: Env.schema.number(),
+  MYSQL_HOST: Env.schema.string({ format: "host" }),
+  MYSQL_PORT: Env.schema.number(),
+  MYSQL_USER: Env.schema.string(),
+  MYSQL_PASSWORD: Env.schema.string(),
+  MYSQL_DB_NAME: Env.schema.string(),
   APP_KEY: Env.schema.string(),
   APP_NAME: Env.schema.string(),
   DRIVE_DISK: Env.schema.enum(["local"] as const),
