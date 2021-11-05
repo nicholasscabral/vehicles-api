b09e1b0ac935d9da6b63ebb9e1e1e48ac9e918c5
nicholasscabral <nicholasscabral@gmail.com>
2021-11-05 17:25:48 -0300

    handleling exceptions on reigister user

--- app/Controllers/Http/AuthController.ts
+++ app/Controllers/Http/AuthController.ts
@@ -4,13 +4,26 @@ export default class AuthController {
   public async register({ request, response }: HttpContextContract) {
     const { email, password } = request.only(["email", "password"]);
 
-    const user = await User.create({ email: email, password: password });
+    if (!email || !password)
+      return response.status(400).json({ message: "credentials are missing" });
 
-    return response.json(user);
+    try {
+      const userAlreadyExists = await User.findBy("email", email);
+
+      if (userAlreadyExists)
+        return response.status(409).json({ message: "User already exists" });
+
+      const user = await User.create({ email: email, password: password });
+
+      return response.status(201).json(user);
+    } catch (err) {
+      console.log("AuthController.register :>> ", err.message);
+      return response.status(500).json({ message: "could not create user" });
+    }
   }
 
   public async login({ request, auth }: HttpContextContract) {
-    const { email, password } = request.only(["email", "password"]);
+    const { email, password } = request.all();
 
     const token = await auth.attempt(email, password, { expiresIn: "2 min" });
 
