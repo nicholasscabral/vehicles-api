201224320cb859c67390b8608f96b39a63d045c9
nicholasscabral <nicholasscabral@gmail.com>
2021-11-08 19:24:35 -0300

    user update

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -16,6 +16,41 @@ export default class UsersController {
     return response.json(user);
   }
 
+  public async update({ request, params, auth, response }: HttpContextContract) {
+    const { id } = params;
+    
+    if (id != auth.user?.id) {
+      return response
+      .status(401)
+      .json({ message: "you can only edit your account" });
+    }
+
+    if (Object.keys(request.all()).length === 0) {
+      return response.status(400).json({message: 'you must edit at least one field'})
+    }
+    
+    const { email, password, currentPassword } = request.all()
+
+    const user = await User.findOrFail(id);
+
+    if (email) user.email = email
+
+    if (password) {
+      if (!currentPassword) {
+        return response.status(400).json({message: "you must provide your current password"})
+      }
+
+      const passwordMatch = await user.vefiryPassword(currentPassword);
+
+      if (passwordMatch) user.password = password;
+      else return response.status(409).json({ message: "Invalid password"})
+    }
+
+    const savedUser = await user.save()
+
+    return savedUser  
+  }
+
   public async destroy({ params, auth, response }: HttpContextContract) {
     const { id } = params;
 
